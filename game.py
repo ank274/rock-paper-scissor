b15a63b4d8aec2a0d030679aaa348bdde0edcd0f
Annie Kong <annie.kong@stern.nyu.edu>
2019-06-09 22:27:20 -0400

    Capture Inputs

--- game.py
+++ game.py
@@ -1,3 +1,21 @@
 #game.py
 
-print("Rock, Paper, Scissors, Shoot!")
+print("Rock, Paper, Scissors, Shoot!") #this is also a comment
+
+#Capture Inputs- create a variable using input function
+
+user_choice = input("Please choose one of the following options: 'rock', 'paper,' or scissors' (without the quotes):")
+print("_________")
+
+print("YOU CHOSE:", user_choice)
+
+
+
+#Validate Inputs
+
+
+#Generate Computer Selection
+
+#Determine the Winner
+
+#Display Final Outputs/ Outcomes 
